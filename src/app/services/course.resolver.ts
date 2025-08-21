94cda5d8c5c17cc2bb2c54d21a0b7e970448dc43
Andrés Quitián <candhres98@outlook.es>
2025-08-20 23:24:42 -0500

    feat: implementation of TransferState (SSR) and some build configurations for prod

--- src/app/services/course.resolver.ts
+++ src/app/services/course.resolver.ts
@@ -1,29 +1,45 @@
-import {Inject, Injectable, PLATFORM_ID} from '@angular/core';
-import {ActivatedRouteSnapshot, Resolve, RouterStateSnapshot} from '@angular/router';
-import {Course} from '../model/course';
-import {Observable, of} from 'rxjs';
-import {CoursesService} from './courses.service';
-import {first, tap} from 'rxjs/operators';
-import {isPlatformServer} from '@angular/common';
+import { inject, Inject, Injectable, makeStateKey, PLATFORM_ID, TransferState } from '@angular/core';
+import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
+import { Course } from '../model/course';
+import { Observable, of } from 'rxjs';
+import { CoursesService } from './courses.service';
+import { first, tap } from 'rxjs/operators';
+import { isPlatformServer } from '@angular/common';
 
 
 @Injectable()
 export class CourseResolver implements Resolve<Course> {
 
-    constructor(private coursesService: CoursesService) {
+  private transferState = inject(TransferState);
 
+  constructor(
+    private coursesService: CoursesService,
+    @Inject(PLATFORM_ID) private platformId: Object,
+  ) { }
+
+  resolve( route: ActivatedRouteSnapshot, state: RouterStateSnapshot ): Observable<Course> {
+    const courseId = route.params['id'];
+
+    const COURSE_KEY = makeStateKey<Course>( `courseKey-${courseId}` );
+
+    if ( this.transferState.hasKey( COURSE_KEY ) ) {
+      // console.log(`Desde el ${ this.platformId } consumo el valor`)
+      const course = this.transferState.get(COURSE_KEY, null);
+      // lo eliminamos del almacenamiento del state transfer
+      this.transferState.remove( COURSE_KEY );
+      return of(course);
+    } else {
+      return this.coursesService.findCourseById( courseId )
+        .pipe(
+          first(),
+          tap( course => {
+            if ( isPlatformServer(this.platformId) ) {
+              // console.log(`Desde el ${ this.platformId } seteo el valor`)
+              this.transferState.set( COURSE_KEY, course );
+            }
+          })
+        );
     }
-
-    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Course> {
-
-        const courseId = route.params['id'];
-
-        return this.coursesService.findCourseById(courseId)
-            .pipe(
-                first()
-            );
-
-
-    }
+  }
 
 }
